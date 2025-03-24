b95869fd8d2ff5a0388a2a56becae603a78f39db
SamWaku <samuelwaku1st@gmail.com>
2025-03-24 15:45:39 +0200

    rename

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("First");
-Console.Write("Second");
+﻿using System;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
